666562321ed8a345539e6b541893ed91a85cb5f9
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-25 12:55:02 +0200

    Crear la clase 'Header » Menus Funcionalidad'

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Alpine JS -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
 
--- secciones/top-bar.php
+++ secciones/top-bar.php
@@ -1,4 +1,4 @@
-<div class="py-3 flex justify-center gap-6 text-xs text-white bg-[#344854]">
+<div class="py-2 flex justify-center gap-6 text-xs text-white bg-[#344854]">
     <div class="flex items-center gap-1">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
              stroke="currentColor" class="w-4 h-4">
