5b0b1e3e9d279d9b54026e4d176e0eccd9de5e10
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-25 19:02:27 +0200

    Crear clase 'Juegos'

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 <?php require_once 'secciones/featured.php'; ?>
 <?php require_once 'secciones/productos.php'; ?>
 <?php require_once 'secciones/banner.php'; ?>
+<?php require_once 'secciones/juegos.php'; ?>
 <?php require_once 'secciones/footer.php'; ?>
 
 </body>
