8de084cc9b5d9415337700e15000adf35f4c5765
Ibon Azkoitia <ibon@kreatidos.com>
2023-10-25 15:23:35 +0200

    Crear la clase 'Destacado'

---
+++ secciones/featured.php
@@ -0,0 +1,38 @@
+<div class="relative">
+    <div class="relative py-32 max-w-7xl mx-auto px-8 z-30">
+        <div class="max-w-[400px] text-white">
+            <h1 class="text-6xl font-semibold leading-tight">Presentamos Meta Quest 3</h1>
+            <p class="my-6 text-lg">Sumérgete en experiencias increíbles con nuestras nuevas e
+                impactantes gafas de realidad mixta, desde 549,99 €.</p>
+            <div class="flex items-center gap-6">
+                <a href="#"
+                   class="py-4 px-8 rounded-full bg-[#1d65c1] hover:bg-[#3d7cce] font-semibold text-sm"
+                >
+                    Comprar ahora
+                </a>
+                <a href="#" class="flex items-center gap-3 font-medium group">
+                    <div class="bg-white p-1 rounded-full">
+                        <svg viewBox="0 0 24 24" fill="currentColor"
+                             aria-hidden="true"
+                             class="text-[#1d65c1] group-hover:text-[#3d7cce] w-8 h-8"
+                             role="img">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                  d="M14.207 11.293a1 1 0 0 1 0 1.414l-3 3a1 1 0 0 1-1.414-1.414L12.086 12 9.793 9.707a1 1 0 0 1 1.414-1.414l3 3z"></path>
+                        </svg>
+                    </div>
+                    <div class="group-hover:underline underline-offset-8">Más información</div>
+                </a>
+            </div>
+        </div>
+
+        <div class="absolute bottom-4 text-white text-sm">Ghostbusters: Rise of the Ghost Lord</div>
+    </div>
+
+    <div class="absolute top-0 z-20 opacity-[96%] bg-gradient-to-r from-gray-800 from-40% bg-transparent w-full h-full">
+
+    </div>
+    <video autoplay loop muted
+           class="absolute top-0 z-10 w-auto min-w-full object-fill max-h-full max-w-none">
+        <source src="/videos/introduccion.mp4" type="video/mp4"
+    </video>
+</div>
